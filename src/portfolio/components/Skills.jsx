27b8f429dd898911270fdaf11760a593c45ed67b
Lucas Chirione <lchirione@gmail.com>
2022-09-20 21:26:57 -0300

    Error en skill

--- src/portfolio/components/Skills.jsx
+++ src/portfolio/components/Skills.jsx
@@ -14,46 +14,50 @@ export const Skills = () => {
     <div className="container-section animate__animated animate__slideInUp">
       <div className="skills__container">
         <section className="section-frontend" style={styleFront}>
-          <h3>Frontend</h3>
+          <h3>Tecnologias</h3>
           <div className="section-skills">
             <h4>
-              <i className="fas fa-check-circle"></i> HTML
-            </h4>
-            <h4>
-              <i className="fas fa-check-circle"></i> CSS / SASS
-            </h4>
-            <h4>
-              <i className="fas fa-check-circle"></i> Javascript
-            </h4>
-            <h4>
-              <i className="fas fa-check-circle"></i> Jquery
+              <i className="fas fa-check-circle"></i> HTML - Javascript - CSS - SASS
             </h4>
             <h4>
               <i className="fas fa-check-circle"></i> Bootstrap
             </h4>
             <h4>
-              <i className="fas fa-check-circle"></i> React / Redux
+              <i className="fas fa-check-circle"></i> React - Redux - Hooks
             </h4>
             <h4>
-              <i className="fas fa-check-circle"></i> Git / Github
+              <i className="fas fa-check-circle"></i> NodeJS - Express
+            </h4>
+            <h4>
+              <i className="fas fa-check-circle"></i> Python
+            </h4>
+            <h4>
+              <i className="fas fa-check-circle"></i> WordPress - WooCommerce
+            </h4>
+            <h4>
+              <i className="fas fa-check-circle"></i> Git - GitHub
             </h4>
           </div>
         </section>
 
         <section className="section-backend" style={styleBack}>
-          <h3>Backend</h3>
+          <h3>Cloud</h3>
           <div className="section-skills">
             <h4>
-              <i className="fas fa-check-circle"></i> NodeJS
+              <i className="fas fa-check-circle"></i> Google Cloud
             </h4>
             <h4>
-              <i className="fas fa-check-circle"></i> Express
+              <i className="fas fa-check-circle"></i> AWS
             </h4>
+            <h3>Base de datos</h3>
             <h4>
-              <i className="fas fa-check-circle"></i> Firebase
+              <i className="fas fa-check-circle"></i> MySQL
             </h4>
             <h4>
               <i className="fas fa-check-circle"></i> Mongodb
+            </h4>
+            <h4>
+              <i className="fas fa-check-circle"></i> Firebase
             </h4>
           </div>
         </section>
